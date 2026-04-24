9c328ecdff4d84b59b1bcac36a89e7728e0292ad
1zhangrui123zhangrui <3118049016@qq.com>
2026-04-24 23:21:11 +0800

    cubemx重新配置
    cubemx重新配置

--- F407_main/Core/Src/main.c
+++ F407_main/Core/Src/main.c
@@ -18,12 +18,14 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "app/app_controller.h"
 
 /* USER CODE END Includes */
 
@@ -88,11 +90,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
+  AppController_Init(&huart1, &huart2);
 
   /* USER CODE END 2 */
 
@@ -103,6 +107,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    AppController_Run();
   }
   /* USER CODE END 3 */
 }
